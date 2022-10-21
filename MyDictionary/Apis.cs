cecbca7148f442b91bec2d8e9c29cbf71434a9b1
Casey Hudler <chudler624@gmail.com>
2022-10-21 15:33:02 -0400

    almost working

---
+++ MyDictionary/Apis.cs
@@ -0,0 +1,51 @@
+﻿using MyDictionary.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MyDictionary
+{
+    public class Apis
+    {
+        public Apis() { }
+
+        public async List<WordInfoModel> GetWordInfo(string word)
+        {
+			var client = new HttpClient();
+			var request = new HttpRequestMessage
+			{
+				Method = HttpMethod.Get,
+				RequestUri = new Uri($"https://wordsapiv1.p.rapidapi.com/words/{word}"),
+				Headers =
+	{
+		{ "X-RapidAPI-Key", "fe96c29976msh7164fa7f072bef1p19efc6jsn0ad83cbe2bb2" },
+		{ "X-RapidAPI-Host", "wordsapiv1.p.rapidapi.com" },
+	},
+			};
+			using (var response = client.SendAsync(request).Result)
+			{
+				response.EnsureSuccessStatusCode();
+				var body = response.Content.ReadAsStringAsync().Result;
+				var wordInfo = JArray.Parse(body)["results"];
+				var wordInfoResults = JsonConvert.DeserializeObject<WordInfoResults>(body);
+
+				List<WordInfoModel> wordInformation = new List<WordInfoModel>();
+				var resultData = wordInfoResults;
+
+				foreach(var item in wordInfo)
+                {
+					var wordInfoModel = new WordInfoModel();
+					wordInfoModel.Definition = (string)item["definition"];
+					wordInfoModel.PartOfSpeech = (string)item["partOfSpeech"];
+					wordInfoModel.Synonyms= item["sysonyms"]
+				
+					
+
+
+				return List<WordInfoModel>;
+			}
+
+		}
+
+
+    }
+}
---
+++ MyDictionary/Result.cs
@@ -0,0 +1,22 @@
+﻿namespace MyDictionary
+{
+    public partial class WordInfoResults
+    {
+        public class Result
+        {
+            public string definition { get; set; }
+            public string partOfSpeech { get; set; }
+            public List<string> synonyms { get; set; }
+            public List<string> typeOf { get; set; }
+            public List<string> hasTypes { get; set; }
+            public List<string> also { get; set; }
+            public List<string> derivation { get; set; }
+            public List<string> hasMembers { get; set; }
+            public List<string> examples { get; set; }
+            public List<string> partOf { get; set; }
+            public List<string> hasSubstances { get; set; }
+        }
+
+
+    }
+}
